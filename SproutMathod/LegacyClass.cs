fd00086f7e8cb154cf7ec07162780d327c1633c0
Jan Polzer <janman74@gmail.com>
2019-11-22 12:49:15 -0600

    Add legacy class

---
+++ SproutMathod/LegacyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SproutMethod
+{
+    public class LegacyClass
+    {
+        public void AppendDictionary<TKey, TValue>(Dictionary<TKey, TValue> fromDict, Dictionary<TKey, TValue> toDict)
+        {
+            foreach (var item in fromDict)
+            {
+                toDict.Add(item.Key, item.Value);
+            }
+        }
+    }
+}
